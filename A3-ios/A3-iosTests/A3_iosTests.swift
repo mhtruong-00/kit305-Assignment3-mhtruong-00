998860eab61c35439159e164a3ff1d89057286a7
mhtruong-00 <mhtruong@utas.edu.au>
2026-05-15 12:40:24 +1000

    update
    Update

---
+++ A3-ios/A3-iosTests/A3_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  A3_iosTests.swift
+//  A3-iosTests
+//
+//  Created by mobiledev on 15/5/2026.
+//
+
+import Testing
+@testable import A3_ios
+
+struct A3_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
