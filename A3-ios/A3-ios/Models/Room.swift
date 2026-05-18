f4e8eee4eea47a40afddb2a526f161d276e7a0ca
mhtruong-00 <mhtruong@utas.edu.au>
2026-05-19 00:27:55 +1000

    models: add houseId/photoBase64/photoUrl to Room

--- A3-ios/A3-ios/Models/Room.swift
+++ A3-ios/A3-ios/Models/Room.swift
@@ -1,16 +1,21 @@
 // With support from GitHub Copilot
+// Aligned with Android app: top-level "rooms" collection, fields:
+// houseId, name, photoBase64, photoUrl
 import Foundation
 
 struct Room {
     var id: String
     var houseId: String
     var name: String
-    var createdAt: Date
+    var photoBase64: String?
+    var photoUrl: String?
 
-    init(id: String = "", houseId: String = "", name: String = "", createdAt: Date = Date()) {
+    init(id: String = "", houseId: String = "", name: String = "",
+         photoBase64: String? = nil, photoUrl: String? = nil) {
         self.id = id
         self.houseId = houseId
         self.name = name
-        self.createdAt = createdAt
+        self.photoBase64 = photoBase64
+        self.photoUrl = photoUrl
     }
 }
